f6bb8b9c0ca052c06c01c1d7742c13cfd5e3f411
Leontii Polovinko <half.leon11@gmail.com>
2020-12-24 14:02:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Just a simple class
+    1. One
+    2. Two
+    3. Three
+    
+This is my *new* **class**!
---
+++ src/ForGit/SimpleClass.java
@@ -0,0 +1,8 @@
+package ForGit;
+
+public class SimpleClass {
+
+    public static void main(String[] args) {
+        System.out.println("First git.");
+    }
+}
