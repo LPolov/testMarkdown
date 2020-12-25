7c012421e9d21ab96af1de815b1eac48be956309
Leontii Polovinko <half.leon11@gmail.com>
2020-12-25 14:54:53 +0300

    New project created

--- src/ForGit/SimpleClass.java
+++ src/ForGit/SimpleClass.java
@@ -3,15 +3,6 @@ package ForGit;
 public class SimpleClass {
 
     public static void main(String[] args) {
-        System.out.println("Now issue is solved.");
-        //int i = 7; deleted in issue
-        System.out.println("New tag.");
-
-
-        // Added in feature 1 commit
-        // int j changed for int i in issue
-        for (int i = 0; i < 1; i++) {
-            System.out.println("Added feature.");
-        }
+        System.out.println("My new project.");
     }
 }
