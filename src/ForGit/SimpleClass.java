c10a1f64f32e1acd9d71ea8387e94f4cb468ec34
Leontii Polovinko <half.leon11@gmail.com>
2020-12-25 14:50:19 +0300

    First issue commit

--- src/ForGit/SimpleClass.java
+++ src/ForGit/SimpleClass.java
@@ -4,12 +4,13 @@ public class SimpleClass {
 
     public static void main(String[] args) {
         System.out.println("Now issue is solved.");
-        int i = 7;
+        //int i = 7; deleted in issue
         System.out.println("New tag.");
 
 
         // Added in feature 1 commit
-        for (int j = 0; i < 1; i++) {
+        // int j changed for int i in issue
+        for (int i = 0; i < 1; i++) {
             System.out.println("Added feature.");
         }
     }
