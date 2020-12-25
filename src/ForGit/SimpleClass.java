30c696e5c224707bb230e330adf6340c29b4ef98
Leontii Polovinko <half.leon11@gmail.com>
2020-12-25 14:55:52 +0300

    New feature is added

--- src/ForGit/SimpleClass.java
+++ src/ForGit/SimpleClass.java
@@ -4,5 +4,8 @@ public class SimpleClass {
 
     public static void main(String[] args) {
         System.out.println("My new project.");
+
+
+        System.out.println("New feature is added.");
     }
 }
