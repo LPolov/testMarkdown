b18005ab9f8b61833f061fb8f29059c5cf0b2507
Leontii Polovinko <half.leon11@gmail.com>
2020-12-25 14:56:42 +0300

    Issue

--- src/ForGit/SimpleClass.java
+++ src/ForGit/SimpleClass.java
@@ -4,5 +4,7 @@ public class SimpleClass {
 
     public static void main(String[] args) {
         System.out.println("My new project.");
+
+        System.out.println("Issue is appeared.");
     }
 }
